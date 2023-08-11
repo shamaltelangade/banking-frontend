6426333b4907b9cb60041d91641c95555c215050
unknown <shamalstelangade@gmail.com>
2023-08-11 14:28:33 +0530

    adding files

---
+++ src/components/Login.js
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import axios from "axios";
+import './Login.css';
+
+const Login = () =>{
+    const baseURL = "https://localhost:8080/checkLogin";
+    const[username, setUsername] = useState('');
+    const[password, setPassword] = useState('');
+
+    const usernameChangeHandler=(event)=>{
+        setUsername(event.target.value) 
+    }
+    const passwordChangeHandler=(event)=>{
+        setPassword(event.target.value) 
+    }
+
+    const submitActionHandler = (event)=>{
+        event.preventDefault();
+        axios
+        .post(baseURL,{
+            username: username,
+            password: password
+        })
+        .then((response)=>{
+            alert(" login success !");
+        })
+        .catch(error => {
+            alert("error==="+error);
+        });
+    };
+    return(
+    <div className="sec">
+        <div className="fbox">
+            <div className="f-value">
+                <form onSubmit={submitActionHandler}> 
+                    <h2>Login</h2>
+                    <div className='input'>
+                        <input type="text" value={username}onChange={usernameChangeHandler} required></input>
+                        <label>Username</label>
+                    </div>
+                    <div className='input'>
+                        <input type="password" value={password} onChange={passwordChangeHandler} required ></input>
+                        <label>Password</label>
+                    </div>
+                    <div className='forget'>
+                        <a href='#'>Forget Password</a>
+                    </div>
+                    <button type='submit'>Log In</button>
+                    <div className='register'>
+                        <p>Don't have a account ? <a href='#'>Register</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    );
+}
+export default Login
