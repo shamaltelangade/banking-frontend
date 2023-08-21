af027976287ba1345824338609923c7bdfa01c5e
unknown <jatinrao361@gmail.com>
2023-08-21 10:07:34 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-<<<<<<< Updated upstream
 import {Routes,Route, BrowserRouter} from 'react-router-dom';
 import LandingPage from './components/landing/LandingPage';
 import Login from './components/landing/Login';
 import Register from './components/landing/Register';
 import Dashboard from './components/dashboard/Dashboard';
 import AccSummary from './components/dashboard/AccSummary';
-=======
-import './App.css';
-import Login from './components/login';
-import Register from './components/Register';
-import Create from './components/CreatePassword';
->>>>>>> Stashed changes
 
 import ForgotPasswordPage from './components/ResetPassword';
 function App() {
   return (
-<<<<<<< Updated upstream
     <BrowserRouter>
     <Routes>
       <Route exact path="/" element={<LandingPage />} ></Route>
@@ -32,12 +24,6 @@ function App() {
       
     </Routes>
   </BrowserRouter>
-=======
-    <div>
-      
-<Create/>
-    </div>
->>>>>>> Stashed changes
   );
 }
 
