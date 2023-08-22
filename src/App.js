613665d5bb2ecaeb1686ecd39ee35139b2b418dd
unknown <rupal.poddar048@gmail.com>
2023-08-22 14:25:03 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import {Routes,Route, BrowserRouter} from 'react-router-dom';
 import LandingPage from './components/landing/LandingPage';
 import Login from './components/landing/Login';
-import Register from './components/landing/Register';
+import Register from './components/Register';
+import ForgotUserIdWithOTP from './components/ForgotUserId';
 import Dashboard from './components/dashboard/Dashboard';
 import AccSummary from './components/dashboard/AccSummary';
-import TransactionForm from './components/TransactionForm';
-import Withdrawl from './components/Withdrawl';
-import SelfTransfer from './components/SelfTransfer';
-import TransferSuccessful from './components/TransferSuccessful';
+import Expired from './components/SessionExpired';
+import ForgotUserIdWithOtp from './components/ForgotUserId';
+import AdminDashboard from './components/SeeTransaction';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route exact path="/" element={<LandingPage />} ></Route>
+      <Route exact path="/" element={<AdminDashboard />} ></Route>
+      {/* <Route path="/login" element={<Login />}/> */}
+      {/* <Route path="/Register" element={<Register />}/> */}
 
-      <Route path="/login" element={<Login />}/>
-      <Route path="/register" element={<Register />}/>
-
-      <Route path ="/dashboard" element={<Dashboard />}/>
-      <Route path ="/accSummary" element={<AccSummary/>}/>
+      {/* <Route path ="/dashboard" element={<Dashboard />}/> */}
+      {/* <Route path ="/accSummary" element={<AccSummary/>}/> */}
       
-      <Route path="/onlineTransfer" element={<TransactionForm/>}/>
-      <Route path="/selfTransfer" element={<SelfTransfer/>}/>
-      <Route path="/cashWithdrawl" element={<Withdrawl/>}/>
-      <Route path="/transferSuccess" element={<TransferSuccessful/>}/>
+      
+      {/* <Route path="/account" element={<Account/>} />
+      <Route path ="/Transaction" element={<Transaction/>}/> */}
+      
     </Routes>
   </BrowserRouter>
   );
--- src/components/landing/Register.js
+++
@@ -1,9 +0,0 @@
-import React, {useState} from 'react';
-
-function Register(){
-    return(
-        <div></div>
-    );
-}
-
-export default Register;
